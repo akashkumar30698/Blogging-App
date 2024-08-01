4e116be93d3213e592ca18694d04a8e80e447df2
akashkumar30698 <144131035+akashkumar30698@users.noreply.github.com>
2024-08-01 21:16:15 +0530

    mmm

--- backend/index.js
+++ backend/index.js
@@ -1,66 +1,126 @@
 const express = require("express");
-const path = require("path");
-const cors = require("cors");
-require("dotenv").config();
-
-const { router: userAuth } = require("./routes/userAuth");
-const { router: home } = require("./routes/userAuth");
-const { router: addBlog } = require("./routes/blog");
-const { connectToDB } = require("./connectToDB");
-
 const app = express();
+const path = require("path"); // For resolving template path
+const {router: userAuth }= require("./routes/userAuth");
+const cors = require('cors');
+const { router: home } = require("./routes/userAuth"); 
+const { connectToDB } = require("./connectToDB");
+const { router: addBlog } = require("./routes/blog")
+require('dotenv').config(); //used for securing data so important credentials cannot be accessed through source code
+
+
+
+
 const port = process.env.PORT || 4000;
 
-// CORS configuration
+/*
+app.use(express.static(path.join(__dirname, '../frontend/index.html')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
+*/
+
+// CORS
+
 const whitelist = [
   "http://localhost:5173", // Local development URL
-  process.env.REACT_APP_URL, //  Production level
-];
+   `${process.env.REACT_API_URL}`       // Production URL
+]; //                 |   DONT CHANGE REACT_API
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true, // Enable cookies and other credentials
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allow all methods
+  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"], // Allow all methods
 };
 
+
 app.use(cors(corsOptions));
+
+app.options('*', cors()) // include before other routes
+
+
+
+
+
+
+
+
+
+
 
 // Middleware to parse frontend data (Body)
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Handle all other routes by serving the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
+// Serve static files from the 'uploads' directory
 
 
+//     Dont change it                                       Give directory name
+//         |                                                  |
+app.use('/uploads', express.static(path.join(__dirname, 'userUploads')));
+  /*    |
+     THIS MIDDLEWARE IS USEFUL FOR POSTING OR RETRIEVING IMAGES FROM BACKEND(DATABASE) TO FRONTEND
+      YOU CAN RETRIEVE IT FROM URL : `http://localhost:8000/uploads/{Image-Name}`
+    ---> REMEMBER YOU CAN ONLY USE IT WITH MULTER
+  */ 
 
 
 
 
-// API routes
+
+// Set view engine to Pug
+app.set("views", path.join(__dirname, "views")); // Replace with your views directory
+app.set("view engine", "pug");
+
+// Home route
 app.use("/", home);
+
+// User authentication routes
 app.use("/", userAuth);
-app.use("/", addBlog);
+
+//ADD a new blog
+app.use("/",addBlog)
+
+
 
 // MongoDB connection
-connectToDB(process.env.MONGODB_CONNECTION)
+connectToDB(`${process.env.MONGODB_CONNECTION}`)
   .then(() => {
     console.log("MongoDB connected");
   })
   .catch((error) => {
     console.error("MongoDB connection error:", error);
   });
+
+
+
+// To fix error "Cannot get /signUp" (Optional to use as error might have occured cuz of proxy in vite.config.js)
+
+
+
+// Serve static files from the React app
+//FIX 1
+app.use(express.static(path.join(__dirname, '../frontend/index.html')));
+
+
+//FIX 2
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend' , 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+
 
 
 // Start the server
